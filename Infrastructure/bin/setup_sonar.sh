241e0c379a7d22813b1144c142bba5c842a0361d
vishnu vardhan m <vishnuvardhan.mamidi-capgemini.c@bastion.682a.example.opentlc.com>
2018-12-07 10:11:52 +0000

    sonarqube setup configured

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -14,3 +14,39 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 # oc new-app -f ../templates/sonarqube.yaml --param .....
 
 # To be Implemented by Student
+# Creating a new PostgreSQL database
+oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db -n $GUID-sonarqube
+
+# Creating a new SonarQube instance from 
+oc new-app docker.io/redhat-gpte-devopsautomation/sonarqube:6.7.4 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube -n $GUID-sonarqube
+
+oc rollout pause dc sonarqube -n $GUID-sonarqube
+
+oc expose svc sonarqube -n $GUID-sonarqube
+
+# Configuring and creating Persistent Volume Claim
+echo "apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: sonarqube-pvc
+spec:
+  accessModes:
+  - ReadWriteOnce
+  resources:
+    requests:
+      storage: 4Gi" | oc create -f - -n $GUID-sonarqube
+
+# Configuring the volumes associated
+oc set volume dc/sonarqube --add --overwrite --name=sonarqube-volume-1 --mount-path=/opt/sonarqube/data/ --type persistentVolumeClaim --claim-name=sonarqube-pvc -n $GUID-sonarqube
+
+# Configuring SonarQube resoruces
+oc set resources dc/sonarqube --limits=memory=3Gi,cpu=2 --requests=memory=2Gi,cpu=1 -n $GUID-sonarqube
+
+# Configuring deployment strategy
+oc patch dc sonarqube --patch='{ "spec": { "strategy": { "type": "Recreate" }}}' -n $GUID-sonarqube
+
+# Setting readiness & liveliness probes for nexus
+oc set probe dc/sonarqube -n $GUID-sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok
+oc set probe dc/sonarqube -n $GUID-sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about
+
+oc rollout resume dc sonarqube -n $GUID-sonarqube
